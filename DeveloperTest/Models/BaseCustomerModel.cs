a2de78d856f8a791e194f00334a9d15b6c5c1898
Thomas Glaser <thomas@tkglaser.net>
2020-08-06 15:34:23 +0100

    Add some basic validation

--- DeveloperTest/Models/BaseCustomerModel.cs
+++ DeveloperTest/Models/BaseCustomerModel.cs
@@ -1,11 +1,15 @@
 using DeveloperTest.Database.Enums;
+using System.ComponentModel.DataAnnotations;
 
 namespace DeveloperTest.Models
 {
     public class BaseCustomerModel
     {
+        [Required]
+        [MinLength(5)]
         public string Name { get; set; }
 
+        [Required]
         public CustomerType Type { get; set; }
     }
 }
--- DeveloperTest/Models/BaseJobModel.cs
+++ DeveloperTest/Models/BaseJobModel.cs
@@ -1,11 +1,14 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace DeveloperTest.Models
 {
     public class BaseJobModel
     {
+        [Required]
         public string Engineer { get; set; }
 
+        [Required]
         public DateTime When { get; set; }
     }
 }
