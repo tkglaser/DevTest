09596862f2c30b3d8df70dd72ed0b4ef176d5d54
Thomas Glaser <thomas@tkglaser.net>
2020-08-06 17:43:13 +0100

    Small fixes and validation

--- ui/src/app/job/job.component.ts
+++ ui/src/app/job/job.component.ts
@@ -1,16 +1,21 @@
-import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
-import { EngineerService } from '../services/engineer.service';
-import { JobService } from '../services/job.service';
-import { JobModel } from '../models/job.model';
-import { JobCreateModel } from '../models/job.createmodel';
+import { Component, OnInit } from "@angular/core";
+import { NgForm } from "@angular/forms";
+import { EngineerService } from "../services/engineer.service";
+import { JobService } from "../services/job.service";
+import { JobModel } from "../models/job.model";
+import { JobCreateModel } from "../models/job.createmodel";
+import { Observable } from "rxjs";
+import { CustomerModel } from "../models/customer.model";
+import { CustomerService } from "../services/customer.service";
+import { shareReplay } from "rxjs/operators";
 
 @Component({
-  selector: 'app-job',
-  templateUrl: './job.component.html',
-  styleUrls: ['./job.component.scss']
+  selector: "app-job",
+  templateUrl: "./job.component.html",
+  styleUrls: ["./job.component.scss"],
 })
 export class JobComponent implements OnInit {
+  public customers$: Observable<CustomerModel[]>;
 
   public engineers: string[] = [];
 
@@ -19,26 +24,36 @@ export class JobComponent implements OnInit {
   public newJob: JobCreateModel = {
     engineer: null,
     when: null,
-    customerId: null
+    customerId: undefined,
   };
 
   constructor(
+    private readonly customerService: CustomerService,
     private engineerService: EngineerService,
-    private jobService: JobService) { }
+    private jobService: JobService
+  ) {}
 
   ngOnInit() {
-    this.engineerService.GetEngineers().subscribe(engineers => this.engineers = engineers);
-    this.jobService.GetJobs().subscribe(jobs => this.jobs = jobs);
+    this.engineerService
+      .GetEngineers()
+      .subscribe((engineers) => (this.engineers = engineers));
+    this.jobService.GetJobs().subscribe((jobs) => (this.jobs = jobs));
+    this.fetchCustomers();
   }
 
   public createJob(form: NgForm): void {
     if (form.invalid) {
-      alert('form is not valid');
+      alert("form is not valid");
     } else {
       this.jobService.CreateJob(this.newJob).then(() => {
-        this.jobService.GetJobs().subscribe(jobs => this.jobs = jobs);
+        this.jobService.GetJobs().subscribe((jobs) => (this.jobs = jobs));
       });
     }
   }
 
+  private fetchCustomers() {
+    this.customers$ = this.customerService
+      .GetCustomers()
+      .pipe(shareReplay({ bufferSize: 1, refCount: true }));
+  }
 }
